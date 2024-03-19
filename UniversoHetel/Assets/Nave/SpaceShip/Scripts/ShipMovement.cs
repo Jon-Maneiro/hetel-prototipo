7b9dfbdb8675734864f4ad604f362a169047d3a9
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2024-03-19 11:15:10 +0100

    Mejoras nave

--- UniversoHetel/Assets/Nave/SpaceShip/Scripts/ShipMovement.cs
+++ UniversoHetel/Assets/Nave/SpaceShip/Scripts/ShipMovement.cs
@@ -12,19 +12,36 @@ using Vector3 = UnityEngine.Vector3;
 public class ShipMovement : MonoBehaviour
 {
     // Mouse variables to rotate
-    private float lookSpeed = 90f;
+    private float lookSpeed = 75f;
     private Vector2 lookInput, screenCenter, mouseDistance;
     
     public InputActions _control;
     [SerializeField] private float forwardThrustPower;
-    [SerializeField] private float maxForwardThrust;
+    [SerializeField] private float maxForwardThrust = 50f;
     [SerializeField] private float horizontalThrustPower;
     private float forwardGlide, HorizontalGlide = 0f;
     [SerializeField] private float speedMult = 1;
     [SerializeField] private float speedMultAngle = 0.5f;
     [SerializeField] private float speedRollMultAngle = 0.05f;
+    [SerializeField] private float throttleIncrement = 0.1f;
+    [SerializeField] private float reponsiveness = 10f;
+    [SerializeField] private float curSpeed;
     private float mouseInputX;
     private float mouseInputY;
+    private float speed;
+
+    private float throttle;
+    private float roll;
+    private float pitch;
+    private float yaw;
+
+    private float responseModifier
+    {
+        get
+        {
+            return (_rigidbody.mass / 10f) * reponsiveness;
+        }
+    }
     
     private Rigidbody _rigidbody;
     private Collider _collider;
@@ -49,13 +66,17 @@ public class ShipMovement : MonoBehaviour
 
     void Update()
     {
+        
+        if (_control.ShipMovement.ForwardMovement.ReadValue<float>() != 0) throttle += throttleIncrement;
+        else if (_control.ShipMovement.HorizontalMovement.ReadValue<float>() != 0) throttle -= throttleIncrement;
+        throttle = Mathf.Clamp(throttle, 0f, 100f);
     }
 
     private void FixedUpdate()
     {
         lookInput.x = Input.mousePosition.x;
         lookInput.y = Input.mousePosition.y;
-
+    
         mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.x;
         mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
         
@@ -65,17 +86,27 @@ public class ShipMovement : MonoBehaviour
     private void ForwardThrust()
     {
         float inputForward = _control.ShipMovement.ForwardMovement.ReadValue<float>();
-        _rigidbody.AddForce(_rigidbody.transform.TransformDirection(Vector3.forward) * inputForward * forwardThrustPower * speedMult * Time.fixedDeltaTime, ForceMode.Force);
-        
+        Debug.Log("inputDorward: " + inputForward);
+        //transform.forward *= inputForward * forwardThrustPower * speedMult * Time.deltaTime;
+        //_rigidbody.AddForce(_rigidbody.transform.TransformDirection(Vector3.forward) * inputForward * maxForwardThrust * throttle * speedMult * Time.deltaTime);
+        Debug.Log("Velocidad: " + _rigidbody.velocity.magnitude);
+        if (_rigidbody.velocity.magnitude >= maxForwardThrust)
+        {
+            _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxForwardThrust);
+        }
+        else
+        {
+            _rigidbody.AddForce(_rigidbody.transform.TransformDirection(Vector3.forward) * inputForward * maxForwardThrust * throttle * speedMult * Time.deltaTime);
+        }
         //_rigidbody.transform.forward += new Vector3(0f, 0f, (inputForward * forwardThrustPower) * Time.deltaTime);
     }
 
     public void HorizontalMoveStart()
     {
         float inputHorizontal = _control.ShipMovement.HorizontalMovement.ReadValue<float>();
-        //_rigidbody.AddTorque(_rigidbody.transform.TransformDirection(Vector3.up) * in);
+        _rigidbody.AddTorque(_rigidbody.transform.TransformDirection(Vector3.forward) * - inputHorizontal * horizontalThrustPower * responseModifier * Time.deltaTime);
         
-        // _rigidbody.AddForce(_rigidbody.transform.TransformDirection(Vector3.right) * inputHorizontal * horizontalThrustPower * speedMult * Time.fixedDeltaTime, ForceMode.Force);
+        //_rigidbody.AddForce(_rigidbody.transform.TransformDirection(Vector3.right) * inputHorizontal * horizontalThrustPower * speedMult * Time.fixedDeltaTime, ForceMode.Force);
         //_rigidbody.AddTorque(_rigidbody.transform.right * speedMultAngle * inputHorizontal, ForceMode.VelocityChange);
     }   
 
