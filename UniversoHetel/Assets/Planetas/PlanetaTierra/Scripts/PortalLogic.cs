f5e9c30c5b8397dbb947665d9a31e6dfa7e95793
Jon Maneiro <32477643+Jon-Maneiro@users.noreply.github.com>
2024-04-16 09:28:39 +0200

    Viaje entre planetas funcional
    Una primera versión del viaje entre planetas, para comprobar que funcione.
    Se ha utilizado el fighter01 como nave, y se ha subido la distancia de renderizado a 10000 de la camara que le sigue.

--- UniversoHetel/Assets/Planetas/PlanetaTierra/Scripts/PortalLogic.cs
+++ UniversoHetel/Assets/Planetas/PlanetaTierra/Scripts/PortalLogic.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PortalLogic : MonoBehaviour
 {
+
+    public string targetScene;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        LoadingData.sceneToLoad = targetScene;
+        SceneManager.LoadScene("LoadingScreen");
     }
 }
