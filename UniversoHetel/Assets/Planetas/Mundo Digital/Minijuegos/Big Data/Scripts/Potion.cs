fbfc3fa5069aa4ed4b16f68b610dce1bc2cb2804
Raúl Estrada <raul6114@gmail.com>
2024-05-06 09:01:59 +0200

    Cambios

---
+++ UniversoHetel/Assets/Planetas/Mundo Digital/Minijuegos/Big Data/Scripts/Potion.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Planetas.Mundo_Digital.Minijuegos.Big_Data.Scripts
+{
+    public class Potion : MonoBehaviour
+    {
+        public PotionType potionType;
+
+        public int xIndex;
+        public int yIndex;
+
+        public bool isMatched;
+        public bool isMoving;
+        
+        private Vector2 _currentPos;
+        private Vector2 _targetPos;
+        
+        [SerializeField] private GameObject particulas;
+        [SerializeField] private Material explotionMat;
+        [SerializeField] private Texture explotionSprite;
+        private bool _started;
+
+        public Potion(int x, int y)
+        {
+            xIndex = x;
+            yIndex = y;
+        }
+
+        public void SetIndices(int x, int y)
+        {
+            xIndex = x;
+            yIndex = y;
+        }
+        
+        public void MoveToTarget(Vector2 targetPosition)
+        {
+            try
+            {
+                StartCoroutine(MoveCoroutine(targetPosition));
+            }
+            catch (MissingReferenceException e)
+            {
+                Debug.Log(e.ToString());
+                SceneManager.LoadScene(0);
+            }
+            
+        }
+
+        private IEnumerator MoveCoroutine(Vector2 targetPosition)
+        {
+            isMoving = true;
+            var duration = 0.2f;
+
+            Vector2 startPosition = transform.position;
+            var elaspedTime = 0f;
+
+            while (elaspedTime < duration)
+            {
+                float t = elaspedTime / duration;
+                transform.position = Vector2.Lerp(startPosition, targetPosition, t);
+                elaspedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = targetPosition;
+            isMoving = false;
+        }
+
+        public void Destruir(bool gameStarted)
+        {
+            _started = gameStarted;
+            Destroy(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            if (!_started) return;
+            GameObject particulass = Instantiate(this.particulas, transform.position, Quaternion.identity);
+            explotionMat.mainTexture = explotionSprite;
+            particulass.GetComponent<ParticleManager>().SetMaterial(explotionMat);
+        }
+    }
+
+    public enum PotionType
+    {
+        Red,
+        Blue,
+        Purple,
+        Green,
+        Yellow,
+        Cyan
+    }
+}
