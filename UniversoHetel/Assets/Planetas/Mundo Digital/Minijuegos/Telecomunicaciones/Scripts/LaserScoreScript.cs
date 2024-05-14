90041e7f38db968d2c00c5303986f9d910921cb3
Jon Maneiro <32477643+Jon-Maneiro@users.noreply.github.com>
2024-05-14 08:57:43 +0200

    Solver Laser Game Error
    Error that prevented the tutorial from being closed.
    Also made transition from that scene into others

--- UniversoHetel/Assets/Planetas/Mundo Digital/Minijuegos/Telecomunicaciones/Scripts/LaserScoreScript.cs
+++ UniversoHetel/Assets/Planetas/Mundo Digital/Minijuegos/Telecomunicaciones/Scripts/LaserScoreScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,41 +23,20 @@ public class LaserScoreScript : MonoBehaviour
         _textoResultado.text = "";
     }
 
+    private void OnDestroy()
+    {
+        GoodCubeScript.SumarPunto -= Add;
+        BadCubeScript.RestarPunto -= Substract;
+    }
+
     private void Add()
     {
         _vida++;
         _goodPoints++;
-        _textoResultado.text = "Succes!";
-        //Invoke(nameof(StopTime), 0.5f);
-        string nombreEscena = SceneManager.GetActiveScene().name;
-        switch (nombreEscena)
-        {
-            case "MinijuegoLaser":
-                LoadingData.SceneToLoad = "MinijuegoLaser 1";
-                SceneManager.LoadScene("LoadingScreen");
-                break;
-            case "MinijuegoLaser 1":
-                LoadingData.SceneToLoad = "MinijuegoLaser 2";
-                SceneManager.LoadScene("LoadingScreen");
-                break;
-            case "MinijuegoLaser 2":
-                LoadingData.SceneToLoad = "MinijuegoLaser 3";
-                SceneManager.LoadScene("LoadingScreen");
-                break;
-            case "MinijuegoLaser 3":
-                LoadingData.SceneToLoad = "MinijuegoLaser 4";
-                SceneManager.LoadScene("LoadingScreen");
-                break;
-            case "MinijuegoLaser 4":
-                LoadingData.SceneToLoad = "SeleccionPlanetas";
-                SceneManager.LoadScene("LoadingScreen");
-                break;
-            default:
-                break;
-            
-        }
+        _textoResultado.text = "Success!";
         
-        
+        Invoke(nameof(Success),2f);
+
     }
 
     private void Substract()
@@ -69,9 +49,7 @@ public class LaserScoreScript : MonoBehaviour
         {
             _textoResultado.text = "Failure!";
             _textoVida.text = "Vida: " + 0;
-            LoadingData.SceneToLoad = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene("LoadingScreen");
-            //Invoke(nameof(StopTime), 0.5f);
+            Invoke(nameof(Failure), 2f);
         }
     }
 
@@ -79,4 +57,20 @@ public class LaserScoreScript : MonoBehaviour
     {
         Time.timeScale = 0;
     }
+
+
+    private void Failure()
+    {
+        LoadingData.SceneToLoad = LoadingData.CurrentScene;
+        SceneManager.LoadScene("LoadingScreen");
+    }
+
+    private void Success()
+    {
+        LoadingData.SceneToLoad = LoadingData.NextContinent;
+        SceneManager.LoadScene("LoadingScreen");
+    }
+
+
+
 }
