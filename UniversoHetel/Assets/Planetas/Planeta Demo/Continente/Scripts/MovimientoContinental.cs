fa707e234a6790584e9d1140b10c008be2fbd172
Raúl Estrada <raul6114@gmail.com>
2024-05-16 08:09:25 +0200

    cambiada luz y movimiento continentes

--- UniversoHetel/Assets/Planetas/Planeta Demo/Continente/Scripts/MovimientoContinental.cs
+++ UniversoHetel/Assets/Planetas/Planeta Demo/Continente/Scripts/MovimientoContinental.cs
@@ -18,26 +18,41 @@ public class MovimientoContinental : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         //Moverse con el teclado
-        float moveAmount = 20f;
+        float moveAmount = 10f;
         if (Input.GetKey(KeyCode.W))
         {
-            cameraPosition.z += moveAmount * Time.deltaTime;
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveAmount);
+            //GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x,transform.position.y, transform.position.z + moveAmount));
+            //cameraPosition = Vector3.Lerp(cameraPosition,
+              //  new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z + 1), moveAmount);
+            //cameraPosition.z += moveAmount * Time.deltaTime;
+            //cameraPosition = Vector3.MoveTowards(cameraPosition, new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z + 1), moveAmount);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            cameraPosition.z -= moveAmount * Time.deltaTime;
+            //cameraPosition.z -= moveAmount * Time.deltaTime;
+            //cameraPosition = Vector3.MoveTowards(cameraPosition, new Vector3(cameraPosition.x, cameraPosition.y, cameraPosition.z -1), moveAmount);
+            GetComponent<Rigidbody>().AddForce(-transform.forward * moveAmount);
+            //GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x,transform.position.y, transform.position.z - moveAmount));
         }
         if (Input.GetKey(KeyCode.A))
         {
-            cameraPosition.x -= moveAmount * Time.deltaTime;
+            //cameraPosition.x -= moveAmount * Time.deltaTime;
+            //cameraPosition = Vector3.MoveTowards(cameraPosition, new Vector3(cameraPosition.x - 1, cameraPosition.y, cameraPosition.z), moveAmount);
+            //GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x - moveAmount, transform.position.y, transform.position.z));
+            GetComponent<Rigidbody>().AddForce(-transform.right * moveAmount);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            cameraPosition.x += moveAmount * Time.deltaTime;
+            //cameraPosition.x += moveAmount * Time.deltaTime;
+            //cameraPosition = Vector3.MoveTowards(cameraPosition, new Vector3(cameraPosition.x + 1, cameraPosition.y, cameraPosition.z), moveAmount);
+            //GetComponent<Rigidbody>().MovePosition(new Vector3(transform.position.x + moveAmount, transform.position.y, transform.position.z));
+            GetComponent<Rigidbody>().AddForce(transform.right * moveAmount);
         }
         
-        //Moverse con los bordes (EdgeScroll)
+        /*//Moverse con los bordes (EdgeScroll)
         float edgeSize = 40f;
 
         if (Input.GetKey(KeyCode.Space))
@@ -66,10 +81,10 @@ public class MovimientoContinental : MonoBehaviour
             {
                 cameraPosition.x -= moveAmount * Time.deltaTime;
             }
-        }
+        }*/
         
         //Actualizar Posicion
-        objectFollow.transform.position = cameraPosition;
+        //objectFollow.transform.position = cameraPosition;
 
     }
 }
